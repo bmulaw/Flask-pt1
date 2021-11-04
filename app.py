0813b0f260392136bbb928dd2660595777387889
Bernard Mulaw <bmulaw@bu.edu>
2021-11-04 05:02:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__, template_folder='template')
 
 @app.route('/',methods =["POST", "GET"])
 def get_artist():
-    name = str(request.form.get("name"))
+    name = request.form.get("name")
     artist = request.form.get("artist")
     return render_template("index.html", name=name, artist=artist)
 
