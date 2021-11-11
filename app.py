6a6378f01a445408fdd97a798fb0c7b47620dcad
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-51.bu.edu>
2021-11-11 11:12:41 -0500

    making api call

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 
 from flask import Flask, render_template, request
-
+import requests, json
+import config
 app=Flask(__name__, template_folder='template')
 
 @app.route('/',methods =["POST", "GET"])
-def get_artist():
-    artist = request.form.get("artist")
-    return render_template("index.html", artist=artist)
+def get_company():
+
+    company = request.form.get("company")
+
+    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
+
+    querystring = {"symbol": company,"region":"US"}
+
+    headers = {
+        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        'x-rapidapi-key': config.api_key
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    obj = response.json()
+    stock_price = obj["price"]['currencySymbol'] + obj["price"]["regularMarketOpen"]["fmt"]
+    
+    return render_template("index.html", company_name=company, price=stock_price)
 
 if __name__ == '__main__':
     app.run(debug=True)
